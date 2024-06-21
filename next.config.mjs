e6a48e5739a4332e749cffb9ed052ebfb8b443d3
Harshil Chandratre <118203817+harshilchandratre@users.noreply.github.com>
2024-06-21 23:44:25 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+// next.config.js
+
+export const images = {
+    domains: ['media.giphy.com'],
+};
+  
